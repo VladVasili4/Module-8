d318ea64d1bc7c23ea5ea2efd786e25e5982de48
VladVasili4 <dipol777@ya.ru>
2024-07-14 16:08:22 +0300

    Add files via upload

---
+++ homework_8-3.py
@@ -0,0 +1,16 @@
+class IncorrectVinNumber:
+    pass
+
+class IncorrectCarNumbers:
+    pass
+
+class Car:
+    def __init__(self, model, vin):
+        self.model = model
+        self.__vin = vin
+
+    def __is_valid_vin(vin_number):
+        pass
+
+    def __is_valid_numbers(numbers):
+        pass
